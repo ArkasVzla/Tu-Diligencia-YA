f6de0ed46772b8006c8672b5e193295203bc148d
Luis Ramos <luisenrique545@gmail.com>
2018-10-10 00:57:35 -0300

    Listado de Solicitudes, Cambios de Estado
    Añade libreria de Alertify para mostrar alertas en respuestas.

--- js/modulo.js
+++ js/modulo.js
@@ -22,7 +22,13 @@ function ingresar_solicitud(){
         data: datos,
         dataType: "json",
         success: function (response) {
-            console.log(response);
+            if(response==1){
+                alertify.success('Su solicitud ha sido ingresada.');
+                $('#id_servicio').val(0);
+                $('#mensaje').val('');
+            } else{ 
+                alertify.error('Ocurrió un error al ingresar la solicitud.');
+            }
         }
     });
 }
@@ -37,8 +43,146 @@ function traer_solicitudes(){
         url: "solicitud.php",
         data: datos,
         dataType: "json",
-        success: function (response) {
-            console.log(response);
+        success: function (resp) {
+            var html = '';
+            for(var key in resp)
+            {
+                var estados = ['<span class="badge badge-warning">En espera</span>', '<span class="badge badge-primary">En Proceso</span>', '<span class="badge badge-success">Aprobado</span>', '<span class="badge badge-danger">Negada</span>'];
+
+                html+='<tr>';
+                html+='<td scope="row">'+resp[key].id+'</td>';
+                html+='<td>'+resp[key].nombre_usuario+'</td>';
+                html+='<td>'+resp[key].nombre+'</td>';
+                html+='<td>'+resp[key].correo+'</td>';
+                html+='<td>'+resp[key].telefono+'</td>';
+                html+='<td>asd</td>';
+                html+='<td>'+estados[resp[key].estado]+'</td>';
+                if(resp[key].estado<=1)
+                {
+                    html+='<td><button type="button" class="btn btn-outline-secondary" onclick="abrir_modal('+resp[key].id+')"><i class="material-icons">open_in_new</i></button></td>';
+                }
+                html+='</tr>';
+                //html+='<td><a href="sesion.php"><img src="" alt="" width="50"></a></td>';
+            }
+            $('#tabla_solicitudes').html(html);
+        }
+    });
+}
+
+function traer_mis_solicitudes(){
+    var datos = {};
+
+    datos.funcion = 'traerTodasSolicitudes';
+    datos.id_usuario = $('#id_usuario').val();
+    
+    $.ajax({
+        type: "POST",
+        url: "solicitud.php",
+        data: datos,
+        dataType: "json",
+        success: function (resp) {
+            var html = '';
+            for(var key in resp)
+            {
+                var estados = ['<span class="badge badge-warning">En espera</span>', '<span class="badge badge-primary">En Proceso</span>', '<span class="badge badge-success">Aprobado</span>', '<span class="badge badge-danger">Negada</span>'];
+
+                html+='<div class="col-sm-12 col-md-6 col-lg-6">';
+                html+='<div class="card border-dark mb-2">';
+                html+='<div class="card-header">'+resp[key].nombre+' #1</div>';
+                html+='<div class="card-body center-block">';
+                html+=estados[resp[key].estado];
+                //html+='<button class="btn btn-outline-success btn-sm disabled" type="submit" value="Enviar">En Espera</button>';
+                if(resp[key].estado<=0)
+                {
+                    html+='<button class="btn btn-outline-danger btn-sm" style="margin-left: 10px;" type="button" value="Eliminar" onclick="eliminar_solicitud('+resp[key].id+')">Eliminar</button>';
+                }
+
+                html+='</div>';
+                html+='</div>';
+                html+='</div>';
+            }
+            $('#tabla_mis_solicitudes').html(html);
+        }
+    });
+}
+
+function eliminar_solicitud(id){
+    var datos = {};
+
+    datos.funcion = 'eliminarSolicitud';
+    datos.id_usuario = $('#id_usuario').val();
+    datos.id = id;
+
+    $.ajax({
+        type: "POST",
+        url: "solicitud.php",
+        data: datos,
+        dataType: "json",
+        success: function (resp) {
+            if(resp==1){
+                traer_mis_solicitudes();
+                alertify.success('Solicitud eliminada.');
+            } else{ 
+                alertify.error('No se pudo eliminar su solicitud.');
+            }
+        }
+    });
+}
+
+function abrir_modal(id)
+{
+    var datos = {};
+
+    datos.funcion = 'traerTodasSolicitudes';
+    datos.id = id;
+    
+    $.ajax({
+        type: "POST",
+        url: "solicitud.php",
+        data: datos,
+        dataType: "json",
+        success: function (resp) {
+            console.log(resp);
+            var html = '';
+            var estado = resp[0].estado;
+            var btn_estilo = ['btn-danger', 'btn-success'];
+            if(estado==0){
+                html+='<button type="button" class="btn '+btn_estilo[1]+' text-center" style="margin-right: 10px;" onclick="cambiar_estado('+id+',1)">Procesar</button>';
+                html+='<button type="button" class="btn '+btn_estilo[0]+' text-center" style="margin-right: 10px;" onclick="cambiar_estado('+id+',3)">Negar</button>';
+            }
+
+            if(estado==1){
+                html+='<button type="button" class="btn '+btn_estilo[1]+' text-center" style="margin-right: 10px;" onclick="cambiar_estado('+id+',2)">Aprobar</button>';
+                html+='<button type="button" class="btn '+btn_estilo[0]+' text-center" style="margin-right: 10px;" onclick="cambiar_estado('+id+',3)">Negar</button>';
+            }
+
+            $('#botones').html(html);
+            $('#cambiar_estado').modal('show');
+        }
+    });
+}
+
+function cambiar_estado(id, estado)
+{
+    var datos = {};
+
+    datos.funcion = 'cambiarEstado';
+    datos.estado = estado;
+    datos.id = id;
+    
+    $.ajax({
+        type: "POST",
+        url: "solicitud.php",
+        data: datos,
+        dataType: "json",
+        success: function (resp) {
+            if(resp==1){
+                traer_solicitudes();
+                alertify.success('Cambio de estado exitoso.');
+                $('#cambiar_estado').modal('hide');
+            } else{ 
+                alertify.error('Ocurrió un problema al cambiar la solicitud.');
+            }
         }
     });
 }
--- solicitud.php
+++ solicitud.php
@@ -15,25 +15,84 @@ if (!$conexion) {
 switch($_POST['funcion'])
 {
     case 'guardar_solicitud':
-        $data = [
-            'id_servicio' => $_POST['id_servicio'],
-            'id_usuario' => $_POST['id_usuario'],
-            'archivo' => 'prueba',
-            'mensaje' => 'mensaje',
-            'estado' => 1,
-        ];
-        $sql = "INSERT INTO solicitudes (id_servicio, id_usuario, archivo, mensaje, estado) VALUES (:id_servicio, :id_usuario, :archivo, :mensaje, :estado)";
-        $stmt= $conexion->prepare($sql);
-        $estado = $stmt->execute($data);    
+        $estado = 1;
+        /*if (!empty($_FILES)) {
+            echo "entra";
+            $targetFolder = 'archivos/';    
+            $tempFile = $_FILES['file']['tmp_name'];    
+            $tempFile = $_FILES['file']['tmp_name'];    
+            //$tempFile = date("YmdHis");    
+            //$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
+            $targetPath = $targetFolder;    
+            $name=str_replace(' ','',$_FILES['file']['name']);    
+            
+            //$targetFile = rtrim($targetPath,'/') . '/' .$numero_imagen.$name;
+            $nombre_imagen = date('YmdGisu');    
+            $fileext = explode('.',$_FILES['file']['name']);    
+            $cantmaxext = count($fileext);    
+            $extensionarchivo = $fileext[$cantmaxext-1];    
+            $targetFile = rtrim($targetPath,'/') . '/' .$nombre_imagen.'.'.$extensionarchivo;
+            // Validate the file type    
+            $fileTypes = array('jpg','jpeg','gif','png','JPG','JPEG','GIF','PNG', 'pdf', 'PDF');
+            // File extensions    
+            //$fileParts = pathinfo($numero_imagen.$name);    
+            $fileParts = pathinfo($name);    
+            if (in_array($fileParts['extension'],$fileTypes)) {        
+                move_uploaded_file($tempFile,$targetFile);        
+                $archivo = $nombre_imagen.'.'.$extensionarchivo;
+                $estado = 1;    
+            } else {        
+                $estado = 0;;    
+            }
+        }*/
+
+        if($estado){
+            $data = [
+                'id_servicio' => $_POST['id_servicio'],
+                'id_usuario' => $_POST['id_usuario'],
+                'archivo' => 'test',
+                'mensaje' => 'mensaje',
+                'estado' => 0,
+            ];
+            $sql = "INSERT INTO solicitudes (id_servicio, id_usuario, archivo, mensaje, estado) VALUES (:id_servicio, :id_usuario, :archivo, :mensaje, :estado)";
+            $stmt= $conexion->prepare($sql);
+            $estado = $stmt->execute($data);   
+        } else {
+            $estado = 0;
+        }
+
+        echo $estado;
     break;
 
     case 'traerTodasSolicitudes':
-        $sql = "SELECT * FROM solicitudes WHERE 1";
+        $filtro = '';
+        if(isset($_POST['id_usuario']))
+            $filtro.=' AND id_usuario='.$_POST['id_usuario'];
+        if(isset($_POST['id']))
+            $filtro.=' AND s.id='.$_POST['id'];
+
+        $sql = "SELECT s.id, s.id_servicio, s.id_usuario, s.archivo, s.mensaje, s.estado, u.nombre as nombre_usuario, u.apellido as apellido_usuario, u.correo, u.cedula, u.telefono, se.nombre FROM solicitudes as s INNER JOIN usuarios as u ON (s.id_usuario=u.id) INNER JOIN servicios as se ON (s.id_servicio=se.id) WHERE 1".$filtro;
         $stmt = $conexion->prepare($sql);
         $stmt->execute();
         $solicitudes = $stmt->fetchAll();
 
         echo json_encode($solicitudes);
+    break;
+
+    case 'eliminarSolicitud':
+        $sql = 'DELETE FROM solicitudes WHERE id='.$_POST['id'].' AND id_usuario='.$_POST['id_usuario']; 
+        $stmt = $conexion->prepare($sql);
+        $estado = $stmt->execute();
+
+        echo $estado;
+    break;
+
+    case 'cambiarEstado':
+        $sql = 'UPDATE solicitudes SET estado='.$_POST['estado'].' WHERE id='.$_POST['id'].''; 
+        $stmt = $conexion->prepare($sql);
+        $estado = $stmt->execute();
+
+        echo $estado;
     break;
 }
 
